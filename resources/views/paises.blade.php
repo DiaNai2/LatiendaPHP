53f05232922a1f765ecd8820e635976fa42e7e6d
Your Name <you@example.com>
2022-05-02 11:28:43 -0500

    evidencia 1

--- resources/views/paises.blade.php
+++ resources/views/paises.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc239Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
@@ -16,25 +16,32 @@
                 <th>Capital</th>
                 <th>Moneda</th>
                 <th>Población</th>
+                <th>Ciudades</th>
         
             </tr>
         </thead>
         <tbody>
             @foreach($paises as $pais => $infopais)
             <tr>
-                <td>
+                <td  class="text-warning" rowspan="{{count($infopais['ciudades'])}}">
                     {{$pais}}
                 </td>
-                <td>
+                <td class="text-info" rowspan="{{count($infopais['ciudades'])}}">
                     {{$infopais["capital"]}}
                 </td>
-                <td>
+                <td class="text-danger" rowspan="{{count($infopais['ciudades'])}}">
                     {{$infopais["moneda"]}}
                 </td>
-                <td>
+                <td class="text-success" rowspan="{{count($infopais['ciudades'])}}">
                     {{$infopais["poblacion"]}}
                 </td>
+            @foreach($infopais["ciudades"] as $ciudad)
+            
+                <td class="bg-secondary text-info"> {{ $ciudad}}</td>
             </tr>
+
+            @endforeach
+            
             @endforeach
         </tbody>
         <tfoot></tfoot>
